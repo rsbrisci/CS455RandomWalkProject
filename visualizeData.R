ec61f70dd92417cd4d5ec41dc2e4f93a181e103c
rsbrisci <rsbrisci@umass.edu>
2016-04-04 18:36:59 -0400

    Fixed graph output in R

--- visualizeData.R
+++ visualizeData.R
@@ -9,7 +9,8 @@ colnames(df) <- c("vertices","edges","algorithm","path_length")
 df <- tbl_df(df)
 df <- arrange(df,edges,vertices)
 
-
+png("plot.png")
 graph <- ggplot(df, aes(x=edges,y=path_length)) + geom_point(size = 0.02) + geom_smooth()
 graph <- graph + ggtitle(paste(df$algorithm, " Performance")) + xlab("Number of Edges") + ylab("Average Path Length")
 graph
+dev.off()
