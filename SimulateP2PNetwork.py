556fa425240e8c2d75a6c024faf347f0f93b6207
rsbrisci <rsbrisci@umass.edu>
2016-04-03 23:29:04 -0400

    Fixed permission error

--- SimulateP2PNetwork.py
+++ SimulateP2PNetwork.py
@@ -2,7 +2,7 @@
 #   Ross Sbriscia, April 2016
 import random
 import sys
-import random_connected_graph
+import os
 # Creates an empty graph with a number of supplied verticies
 
 
@@ -14,6 +14,7 @@ def createNetwork(numVerticies):
         index += 1;
     return network;
 
+os.system("script2.py 1")
 # Returns a connected graph with randomized edges.
 # This simulates the reality of real p2p networks,
 # as hosts very often come online and go offline.
