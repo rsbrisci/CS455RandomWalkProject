2b862c07df56d4906b9d49592bae946d18239413
rsbrisci <rsbrisci@umass.edu>
2016-04-03 23:17:59 -0400

    refined credit statement

--- SimulateP2PNetwork.py
+++ SimulateP2PNetwork.py
@@ -2,7 +2,10 @@
 #   Ross Sbriscia, April 2016
 import random
 import sys
+import random_connected_graph
 # Creates an empty graph with a number of supplied verticies
+
+
 def createNetwork(numVerticies):
     network = {};
     index = 0;
@@ -14,9 +17,3 @@ def createNetwork(numVerticies):
 # Returns a connected graph with randomized edges.
 # This simulates the reality of real p2p networks,
 # as hosts very often come online and go offline.
-def randomizeEdges(network):
-    for node in network.iteritems():
-        node.append()
-    return network;
-
-randomizeEdges(createNetwork(4));
