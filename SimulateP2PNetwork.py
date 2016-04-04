307f159486b9739bf6ac8f9a656fd6aa9c57cb63
rsbrisci <rsbrisci@umass.edu>
2016-04-03 21:11:11 -0400

    Added SimulatePython file, this will (eventually) return our data to be interpreted by an as-of-yet nonexistant R script

---
+++ SimulateP2PNetwork.py
@@ -0,0 +1,22 @@
+# Author:
+#   Ross Sbriscia, April 2016
+import random
+import sys
+# Creates an empty graph with a number of supplied verticies
+def createNetwork(numVerticies):
+    network = {};
+    index = 0;
+    while (index < numVerticies):
+        network[index] = [];
+        index += 1;
+    return network;
+
+# Returns a connected graph with randomized edges.
+# This simulates the reality of real p2p networks,
+# as hosts very often come online and go offline.
+def randomizeEdges(network):
+    for node in network.iteritems():
+        print node;
+    return network;
+
+randomizeEdges(createNetwork(4));
