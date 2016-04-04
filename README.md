69bd516570d68ccd939849dc630437021a37059e
rsbrisci <rsbrisci@umass.edu>
2016-04-04 10:17:20 -0400

    JK Actually made the README make sense

--- README.md
+++ README.md
@@ -1,17 +1,52 @@
 # CS455RandomWalkProject
-*NOT YET FEATURE COMPLETE*
+NOT YET FEATURE COMPLETE
 
 The purpose of this project is to examine the performance consequences of using random-walk based algorithms in p2p filesharing networks.
 
 ## Requirements
-1. Python
-⋅⋅⋅ https://www.python.org/downloads/
-2. R-Base
-⋅⋅⋅ https://cran.cnr.berkeley.edu/
+1. Python https://www.python.org/downloads/
+2. R-Base https://cran.cnr.berkeley.edu/
 
 ## Usage
 ```
-python SimulateP2PNetwork.py <Number of Vertices>
+python SimulateP2PNetwork.py -h
+
+usage: SimulateP2PNetwork.py [-h] [-t T] [-e E] vertices {randomwalk,normal}
+
+Simulates a P2P network with random dynamic connectivity and examines runtime and space complexity of varius algorithms.
+
+positional arguments:
+  vertices             Number of vertices in the simulated network (Recommend
+                       <= 1000)
+  {randomwalk,normal}  Choose an algorithm to use in the simulation
+
+optional arguments:
+  -h, --help           show this help message and exit
+  -t T                 (Default 50) Number of trials to run per graph (with
+                       same edges)
+  -e E                 (Default 100) Number of times graph will be re-built
+                       with new edges
+
+Examples:
+
+python SimulateP2PNetwork.py 30 randomwalk
+This will simulate a network of 30 vertices and use the random walk algorithm.
+
+python SimulateP2PNetwork.py 500 normal -t 20
+This will simulate a network of 500 verticies, using the normal algorithm, and run a new trial (assign new start and end nodes) on each graph 20 times.
+
+python SimulateP2PNetwork.py 350 randomwalk -t 30 -e 200
+This will simulate a network of 500 verticies, using the normal algorithm, run a new trial (assign new start and end nodes) on each graph 30 times and re-build (assign new edges) the graph 200 times.
+
+Output: a csv in the following form (one line per run);
+num vertices, num edges, algorithm used, length of path found, space needed per host (in vertices)
+Ex:
+300,543,randomwalk,102,40
+300,543,randomwalk,34,13
+300,50000,randomwalk,4000,443
+.
+.
+.
 ```
 
 ###### Credit Github User: bwbaugh for the random connected graph generation script "random_connected_graph.py"!
--- SimulateP2PNetwork.py
+++ SimulateP2PNetwork.py
@@ -1,29 +1,67 @@
 # Author:
-#   Ross Sbriscia, April 2016
+# Ross Sbriscia, April 2016
 import random
 import sys
 import traceback
 import os
 import math
+import argparse
 
+# Parses Arguments
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+description='Simulates a P2P network with random dynamic connectivity in order to examines runtime \
+and space complexity of search algorithms.',
+epilog="Examples:\n\
+\n\
+python SimulateP2PNetwork.py 30 randomwalk\n\
+This will simulate a network of 30 vertices and use the random walk algorithm.\n\
+\n\
+python SimulateP2PNetwork.py 500 normal -e 20\n\
+This will simulate a network of 500 verticies, using the normal algorithm, and run a \
+new experiment (assign new start and end nodes) on each graph 20 times.\n\
+\n\
+python SimulateP2PNetwork.py 350 randomwalk -t 30 -e 200\n\
+This will simulate a network of 500 verticies, using the normal algorithm, run a \
+new trial (assign new start and end nodes) on each graph 30 times and re-build (assign new edges) the graph 200 times.\n\
+\n\
+Output: a csv in the following form (one line per RUN);\n\
+num vertices, num edges, algorithm used, average length of path found, space needed per host (in vertices)\n\
+Ex:\n\
+300,543,randomwalk,102,40\n\
+300,543,randomwalk,34,13\n\
+300,1120,randomwalk,3,443\n\
+.\n\
+.\n\
+.")
+parser.add_argument('vertices', type=int,
+                   help='Number of vertices in the simulated network (Recommend <= 1000)')
+parser.add_argument('algorithm',choices=['randomwalk', 'normal'],
+                   help='Choose an algorithm to use in the simulation')
+parser.add_argument('-r', type=int,
+                   help='(Default 10) Number of RUNS per EXPERIMENTS (exact same start and end nodes, on network with same edges)')
+parser.add_argument('-e', type=int,
+                   help='(Default 50) Number of EXPERIMENTS per TRIAL (new start and end nodes, on network with same edges)')
+parser.add_argument('-t', type=int,
+                   help='(Default 100) Number of TRIALS (times graph will be re-built with new edges)')
+args = parser.parse_args()
+# Code Starts Here!
 
+# Returns the maximum possible number of edges of an undirected graph with n verticies
 def maxEdges(n):
-    return (n * (n - 1)) / 2
+    return (n * (n - 1)) / 2;
 
 # Returns a connected graph with randomized edges.
 # This simulates the reality of real p2p networks,
 # as hosts very often come online and go offline.
-
-
 def generateNetwork(numVerticies):
-    edges = random.randrange(numVerticies - 1, maxEdges(numVerticies))
-    print "Generating a graph with %d vertices and %d edges..." % (numVerticies, edges)
+    edges = random.randrange(numVerticies - 1, maxEdges(numVerticies));
+    print "Generating a graph with %d vertices and %d edges..." % (numVerticies, edges);
     os.system("python random_connected_graph.py -p -e " +
-              str(edges) + " " + str(numVerticies) + " > edges.txt")
-    print "Done!"
+              str(edges) + " " + str(numVerticies) + " > edges.txt");
+    print "Done!";
 
 try:
-    generateNetwork(int(sys.argv[1]))
+    generateNetwork(int(sys.argv[1]));
 except (ValueError, IndexError):  # In case of keyboard cat
-    print '\033[91m' +"\nThis call takes a single integer (Number of Verticies) as argument\n" + '\033[0m'
-    traceback.print_exc(file=sys.stdout)
+    print '\033[91m' +"\nThis call takes a single integer (Number of Verticies) as argument\n" + '\033[0m';
+    traceback.print_exc(file=sys.stdout);
