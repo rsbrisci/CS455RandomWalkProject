8a3501c8115e076d466d0d447de1f9c9a5f5c15a
rsbrisci <rsbrisci@umass.edu>
2016-04-06 01:48:09 -0400

    Unbroke the previously fine prog. bar... its 0147, cut me some slack.

--- visualizeData.R
+++ visualizeData.R
@@ -3,7 +3,7 @@
   require("ggplot2")
   require("foreach")
   args <- commandArgs(TRUE)
-  filename <- "RandomWalkSimulation.csv"
+  filename <- "this.csv"
   df <- read.csv(filename)
   colnames(df) <- c("vertices","edges","algorithm","path_length","included_failure")
   df <- tbl_df(df)
@@ -13,7 +13,7 @@
   #png(paste(df$vertices, df$algorithm, " Performance", ".png"))
   
   graph <- ggplot(df, aes(x=edges,y=path_length)) + geom_line(color="grey") + geom_jitter(size = .2) + geom_smooth(se=TRUE)
-  graph <- graph + ggtitle(paste(df$vertices, " ",df$algorithm, " Performance"))+ xlab("Number of Edges") + ylab("Average Path Length")
+  graph <- graph + ggtitle(paste(df$vertices, "-Vertex ",df$algorithm, " Performance"))+ xlab("Number of Edges") + ylab("Average Path Length")
   graph <- graph + theme_minimal() + scale_size_area() + scale_colour_hue(l=20) + geom_vline(data=failureDf,color="blue",size=.2,aes(xintercept=edges))
   graph
   #dev.off()
