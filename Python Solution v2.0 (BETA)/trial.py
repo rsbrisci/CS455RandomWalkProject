567ae14fd182b6f8ce692a102acb837e9f32c071
rsbrisci <rsbrisci@umass.edu>
2016-04-10 05:28:14 -0400

    Fixed argparse for v2.0

--- Python Solution v2.0 (BETA)/trial.py
+++ Python Solution v2.0 (BETA)/trial.py
@@ -12,8 +12,81 @@ in case of frequent link failures.
 import fnss
 import random
 import sys
+import argparse
+import traceback
+import time
+import math
+
+# Parses Arguments
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description='Simulates a P2P network with random dynamic connectivity in order to examines runtime \
+and space complexity of search algorithms.',
+                                 epilog="Examples:\n\
+\n\
+python SimulateP2PNetwork.py 30 randomwalk -o output\n\
+This will simulate a network of 30 vertices and use the random walk algorithm, outputs in output.csv\n\
+\n\
+python SimulateP2PNetwork.py 500 bfs -e 20\n\
+This will simulate a network of 500 verticies, using the BFS algorithm, and run a \
+new experiment (assign new start and end nodes) on each graph 20 times.\n\
+\n\
+python SimulateP2PNetwork.py 350 randomwalk -e 30 -t 200\n\
+This will simulate a network of 500 verticies, using the randomwalk algorithm, run a \
+new trial (assign new start and end nodes) on each graph 30 times and re-build (assign new edges) the graph 200 times.\n\
+\n\
+Output: a csv in the following form (one line per experiment);\n\
+num vertices, num edges, algorithm used, average length of path found, if file NEVER found, average data per hop (bytes), runningtime (seconds)\n\
+Ex:\n\
+250,10898,randomwalk,32373,False,32,3.237650\n\
+250,10898,randomwalk,25520,False,32,2.553203\n\
+250,10898,randomwalk,28501,False,32,2.851121\n\
+.\n\
+.\n\
+.")
+parser.add_argument('vertices', type=int,
+                    help='Number of vertices in the simulated network (Recommend <= 1000)')
+parser.add_argument('algorithm', choices=['randomwalk', 'bfs', 'lazyrandomwalk'],
+                    help='Choose an algorithm to use in the simulation')
+parser.add_argument('-r', type=int,
+                    help='(Default 10) Number of RUNS per EXPERIMENTS (exact same start and end nodes, on network with same edges)')
+parser.add_argument('-e', type=int,
+                    help='(Default 50) Number of EXPERIMENTS per TRIAL (new start and end nodes, on network with same edges)')
+parser.add_argument('-t', type=int,
+                    help='(Default 100) Number of TRIALS (times graph will be re-built with new edges)')
+parser.add_argument('-o',
+                    help='Specify output filename')
+args = parser.parse_args()
+numberOfVertices = args.vertices
+algorithm = args.algorithm
+numberOfFailiures = 0
+maxPathLength = 4 * (math.pow(numberOfVertices, 1))
+if args.t:
+    numberOfTrails = args.t
+else:
+    numberOfTrails = 100
+
+if args.o:
+    outfileName = "./Data/" + args.o + ".csv"
+else:
+    if (algorithm == "randomwalk"):
+        outfileName = "../Data/RandomWalkSimulation.csv"
+    if (algorithm == "bfs"):
+        outfileName = "../Data/BFSSimulation.csv"
+    if (algorithm == "lazyrandomwalk"):
+        outfileName = "../Data/LazyRandomWalkSimulation.csv"
+
+if args.e:
+    numberOfExperiments = args.e
+else:
+    numberOfExperiments = 50
+
+if args.r:
+    numberOfRuns = args.r
+else:
+    numberOfRuns = 10
+
 # generate a Waxman1 topology with 200 nodes
-topology = fnss.waxman_1_topology(n=args[1], alpha=0.4, beta=0.1, L=1)
+topology = fnss.waxman_1_topology(n=numberOfVertices, alpha=0.4, beta=0.1, L=1)
 
 # assign constant weight (1) to all links
 fnss.set_weights_constant(topology, 1)
